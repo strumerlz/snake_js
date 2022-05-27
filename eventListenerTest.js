0ed9e97f6b816f2078008d71fc204fa21ce05b16
Ivan Filichev <ivndfil@gmail.com>
2022-05-28 00:34:41 +0300

    squre mover test

---
+++ eventListenerTest.js
@@ -0,0 +1,5 @@
+
+function log_event(event) { 
+    console.log(`${event.key} key was pressed`);
+}
+document.addEventListener("keydown", log_event);
--- square.js
+++ square.js
@@ -1,46 +1,80 @@
 {
-const canvas = document.getElementById('square');
-const ctx = canvas.getContext('2d');
-const height = canvas.height;
+  const canvas = document.getElementById('square');
+  const ctx = canvas.getContext('2d');
+  const height = canvas.height;
 
-let square = {
-  context: ctx,
-  x: 0,
-  y: 0,
-  size: 10,
-  strokeStyle : 'red',
-  fillStyle: 'orange',
-  lineWidth: 1,
+  let square = {
+    context: ctx,
+    x: 10,
+    y: 50,
+    size: 10,
+    strokeStyle: 'red',
+    fillStyle: 'white',
+    lineWidth: 1,
 
-  stroke() {
-    this.context.strokeStyle = this.strokeStyle;
-    this.context.lineWidth = this.lineWidth;
-    this.context.strokeRect(this.x, this.y, this.size, this.size);
-  },
-  fill() {
-    this.context.fillStyle = this.fillStyle;
-    this.context.fillRect(this.x, this.y, this.size, this.size);
-  },
-  render() {
+    stroke() {
+      this.context.strokeStyle = this.strokeStyle;
+      this.context.lineWidth = this.lineWidth;
+      this.context.strokeRect(this.x, this.y, this.size, this.size);
+    },
+    fill() {
+      this.context.fillStyle = this.fillStyle;
+      this.context.fillRect(this.x, this.y, this.size, this.size);
+    },
+    render() {
       this.fill();
       this.stroke();
-  },
-}
-
-let square1 = Object.create(square);
+    },
+  }
 
 
-square1.size = height / 5;
-square1.strokeStyle = 'green';
-square1.lineWidth = 5;
-square1.context = ctx;
 
-[square1.x, square1.y] = [50, 10];
-square1.stroke();
-[square1.x, square1.y] = [300, 50];
-square1.stroke();
-[square1.x, square1.y] = [250, 350];
-square1.stroke();
-[square1.x, square1.y] = [180, 40];
-square1.stroke();
+ 
+
+  let square1 = Object.create(square);
+
+  square1.size = height / 20 - 2;
+  square1.strokeStyle = 'green';
+  square1.lineWidth = 2;
+  square1.context = ctx;
+  
+
+ let mover = {
+    direction: {
+      x: 1,
+      y: 0,
+    },
+    speed: 6,
+    stepCalc(obj) {
+
+      let dx = obj.size * this.direction.x;
+      let dy = obj.size * this.direction.y;
+      obj.x += dx;
+      obj.y += dy;
+
+    },
+
+    on(obj) {
+      let timeOut = 1000/this.speed;
+      setTimeout(() => {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        this.stepCalc(obj);
+        obj.render();
+        this.on(obj);
+      }, timeOut)
+    },
+  };
+
+  [square1.x, square1.y] = [60, 60];
+
+  square1.render();
+ mover.on(square1);
+
+  
+
+
+  //window.requestAnimationFrame(move.on.call(move));
+
+
+
 }
