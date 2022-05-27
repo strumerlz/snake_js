19301b99eddb1e286f70f0dc8d48915330ebcf1b
Ivan Filichev <ivndfil@gmail.com>
2022-05-27 17:40:50 +0300

    add render method, replace fill/stroke by fillRect/fillStroke

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const canvas = document.getElementById('canvas');
+const canvas = document.getElementById('snake');
 const ctx = canvas.getContext('2d');
 const height = canvas.height; 
 ctx.lineWidth = 2;
--- square.js
+++ square.js
@@ -1,4 +1,4 @@
-const canvas = document.getElementById('canvas');
+const canvas = document.getElementById('square');
 const ctx = canvas.getContext('2d');
 const height = canvas.height;
 
@@ -7,30 +7,38 @@ let square = {
   x: 0,
   y: 0,
   size: 10,
-  strokeStyle: this.context.strokeStyle,
-  fillStyle: this.context.fillStyle,
+  strokeStyle : 'red',
+  fillStyle: 'orange',
   lineWidth: 1,
 
-  stroke: function() {
+  stroke() {
     this.context.strokeStyle = this.strokeStyle;
     this.context.lineWidth = this.lineWidth;
-    this.context.rect(this.x, this.y, this.x + this.size, this.y + this.size);
-    this.context.stroke();
+    this.context.strokeRect(this.x, this.y, this.size, this.size);
   },
-  fill: function() {
+  fill() {
     this.context.fillStyle = this.fillStyle;
-    this.context.rect(this.x, this.y, this.x + this.size, this.y + this.size);
-    this.context.fill();
+    this.context.fillRect(this.x, this.y, this.size, this.size);
+  },
+  render() {
+      this.fill();
+      this.stroke();
   },
 }
 
 let square1 = Object.create(square);
 
 
-square1.size = height / 40;
+square1.size = height / 5;
 square1.strokeStyle = 'green';
-square1.lineWidth = 2;
+square1.lineWidth = 5;
 square1.context = ctx;
 
-[square1.x, square1.y] = [10, 10];
+[square1.x, square1.y] = [50, 10];
+square1.stroke();
+[square1.x, square1.y] = [300, 50];
+square1.stroke();
+[square1.x, square1.y] = [250, 350];
+square1.stroke();
+[square1.x, square1.y] = [180, 40];
 square1.stroke();
