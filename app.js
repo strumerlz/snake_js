b54e97b17b599532328556d97c7b90172d9a12d0
Ivan Filichev <ivndfil@gmail.com>
2022-05-28 19:00:18 +0300

    temp commit. doesnt work properly

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 
 const canvas = document.getElementById('snake');
 const ctx = canvas.getContext('2d');
-const lineWidth = 4;
-const cellSize = canvas.height/20-lineWidth;
+const cellSize = canvas.height/20;
 
 let cellDraw = {
   context: ctx,
   x: 0,
   y: 0,
-  size: cellSize,
-  strokeStyle : 'DarkSlateGray',
-  fillStyle: 'DarkSeaGreen',
-  lineWidth: lineWidth,
-
+  lineWidth: 4,
+  size: cellSize-this.lineWidth,
+  strokeStyle : 'darkSlateGray',
+  fillStyle: 'darkSeaGreen',
+  
   stroke() {
     this.context.strokeStyle = this.strokeStyle;
     this.context.lineWidth = this.lineWidth;
@@ -27,18 +26,64 @@ let cellDraw = {
       this.stroke();
   },
 }
+ let cellSnake = Object.create(cellDraw);
+ let cellFood = Object.create(cellDraw);
+ cellFood.strokeStyle = 'maroon';
+ cellFood.fillStyle = 'chocolate';
 
 let snake = {
+  cell: cellSnake,
   head: [320, 200],
-  tail: [[320-cellSize, 200],
-         [320-cellSize*2, 200],
-         [320-cellSize*3, 200]],
-  render(cell){
+  tail: [[320-this.cell.size, 200],
+         [320-this.cell.size*2, 200],
+         [320-this.cell.size*3, 200]],
+  direction: { x: 1, y: 0},
+  headStepCalc() {
+    let dx = this.cell.size * this.direction.x;
+    let dy = this.cell.size * this.direction.y;
+    this.head[0] += dx;
+    this.cell[1] += dy;
+  },
+  tailStepCalc() {
+    this.tail.unshift(this.head);
+    this.tail.pop()
+  },
+  move(){
+    this.headStepCalc();
+    this.tailStepCalc();    
+  },
+  changeDirection(event) {
+    if (this.direction.x === 0) {
+      switch (event.key) {
+        case 'ArrowLeft':
+          [this.direction.x, this.direction.y] = [-1, 0];
+          break;
+        case 'ArrowRight':
+          [this.direction.x, this.direction.y] = [1, 0];
+          break;
+        default:
+      };
+    } else {
+      switch (event.key) {
+        case 'ArrowUp':
+          [this.direction.x, this.direction.y] = [0, -1];
+          break;
+        case 'ArrowDown':
+          [this.direction.x, this.direction.y] = [0, 1];
+          break;
+        default:
+      };
+    };
+  },
+  render(){
     for (let partLocation of [this.head,...this.tail]){
-      [cell.x, cell.y]=partLocation;
-      cell.render();
+      [this.cell.x, this.cell.y]=partLocation;
+      this.cell.render();
     };
   },  
 }
 
-snake.render(cellDraw);
+//console.log([snake.head, ...snake.tail]);
+//snake.move();
+//console.log([snake.head, ...snake.tail]);
+//snake.render(cellSnake);
