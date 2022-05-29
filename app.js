ef15e346e4d801d9e2860ada96dc20b00a3c21e3
Ivan Filichev <ivndfil@gmail.com>
2022-05-29 13:04:22 +0300

    replace props that duplicate board props

--- app.js
+++ app.js
@@ -1,32 +1,69 @@
 const canvas = document.getElementById('snake');
 const ctx = canvas.getContext('2d');
 
+let board = {
+  canvas: document.getElementById('snake'),
+  ctx: document.getElementById('snake').getContext('2d'),
+  get cellSize() {
+    return this.canvas.height / 20;
+  },
+  get leftBorder() {
+    return 0 - cellSnake.cellSize;
+  },
+  get rightBorder() {
+    return this.canvas.width;
+  },
+  get topBorder() {
+    return 0 - this.cellSize;
+  },
+  get bottomBorder() {
+    return this.canvas.height;
+  },
+  clearCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  },
+  snapToGrid(coordinate, cellSize) {
+    let remainder = coordinate % cellSize;
+    let halfStep = cellSize / 2;
+    if (remainder < halfStep) {
+      return coordinate - remainder;
+    } else {
+      return coordinate - remainder + cellSize;
+    }
+  },
+  genXY() {
+    let x = Math.random() * this.rightBorder;
+    let y = Math.random() * this.bottomBorder;
+    return [x, y];
+  },
+}
+
 let cellDraw = {
-  context: ctx,
+  context: board.ctx,
   x: 0,
   y: 0,
-  cellSize: canvas.height / 20,
   lineWidth: 4,
   get strokeSize() {
-    return this.cellSize - this.lineWidth;
+    return board.cellSize - this.lineWidth;
   },
   strokeStyle: 'DarkSlateGray',
   fillStyle: 'DarkSeaGreen',
 
   stroke() {
-    this.context.strokeStyle = this.strokeStyle;
-    this.context.lineWidth = this.lineWidth;
-    this.context.strokeRect(this.x, this.y, this.strokeSize, this.strokeSize);
+    board.ctx.strokeStyle = this.strokeStyle;
+    board.ctx.lineWidth = this.lineWidth;
+    board.ctx.strokeRect(this.x, this.y, this.strokeSize, this.strokeSize);
   },
   fill() {
-    this.context.fillStyle = this.fillStyle;
-    this.context.fillRect(this.x, this.y, this.strokeSize, this.strokeSize);
+    board.ctx.fillStyle = this.fillStyle;
+    board.ctx.fillRect(this.x, this.y, this.strokeSize, this.strokeSize);
   },
   render() {
     this.fill();
     this.stroke();
   },
-}
+};
+
 let cellSnake = Object.create(cellDraw);
 let cellFood = Object.create(cellDraw);
 cellFood.strokeStyle = '#2f2f4f';
@@ -45,14 +82,14 @@ let snake = {
     y: 0
   },
   init() {
-    for (let i = 1; i < 15; i++) {
-      let arr = [this.head[0] - this.cell.cellSize * i, this.head[1]];
+    for (let i = 1; i < 5; i++) {
+      let arr = [this.head[0] - board.cellSize * i, this.head[1]];
       this.tail.push(arr);
     }
   },
   headStepCalc() {
-    let dx = this.cell.cellSize * this.direction.x;
-    let dy = this.cell.cellSize * this.direction.y;
+    let dx = board.cellSize * this.direction.x;
+    let dy = board.cellSize * this.direction.y;
     this.head[0] += dx;
     this.head[1] += dy;
   },
@@ -175,27 +212,22 @@ const game = {
     document.addEventListener('keydown', (event) => that.pausePlaySwitch(event));
   },
   checkBoundaries() {
-    let leftBorder = 0 - cellSnake.cellSize;
-    let rightBorder = canvas.width;
-    let topBorder = 0 - cellSnake.cellSize;
-    let bottomBorder = canvas.height;
-
-    if (snake.direction.x !== 0) {
+      if (snake.direction.x !== 0) {
       switch (snake.head[0]) {
-        case leftBorder:
-          snake.head[0] = rightBorder - cellSnake.cellSize;
+        case board.leftBorder:
+          snake.head[0] = board.rightBorder - board.cellSize;
           break;
-        case rightBorder:
+        case board.rightBorder:
           snake.head[0] = 0;
           break;
         default:
       }
     } else {
       switch (snake.head[1]) {
-        case topBorder:
-          snake.head[1] = bottomBorder - cellSnake.cellSize;
+        case board.topBorder:
+          snake.head[1] = board.bottomBorder - board.cellSize;
           break;
-        case bottomBorder:
+        case board.bottomBorder:
           snake.head[1] = 0;
           break;
         default:
@@ -210,43 +242,9 @@ const game = {
   }
 }
 
-let board = {
-  canvas: document.getElementById('snake'),
-  ctx: document.getElementById('snake').getContext('2d'),
-  get cellSize() {
-    return this.canvas.height / 20;
-  },
-  get leftBorder() {
-    return 0 - cellSnake.cellSize;
-  },
-  get rightBorder() {
-    return this.canvas.width;
-  },
-  get topBorder() {
-    return 0 - cellSnake.cellSize;
-  },
-  get bottomBorder() {
-    return this.canvas.height;
-  },
 
-  clearCanvas() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-  },
-  snapToGrid(coordinate, cellSize) {
-    let remainder = coordinate % cellSize;
-    let halfStep = cellSize / 2;
-    if (remainder < halfStep) {
-      return coordinate - remainder;
-    } else {
-      return coordinate - remainder + cellSize;
-    }
-  },
-  genXY() {
-    let x = Math.random() * this.rightBorder;
-    let y = Math.random() * this.bottomBorder;
-    return [x, y];
-  },
-}
+
+
 
 
 game.init();
